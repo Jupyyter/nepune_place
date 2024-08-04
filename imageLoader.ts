f982a6c042ee11a7c9a85ac91da1ddf02590d1a2
nepunePlace <raducea.matei2005@gmail.com>
2024-08-04 16:23:41 +0300

    j

--- imageLoader.ts
+++ imageLoader.ts
@@ -1,7 +1,7 @@
 import { ImageLoaderProps } from 'next/image';
 
 const imageLoader = ({ src, width, quality }: ImageLoaderProps): string => {
-  return `/images/${src}?w=${width}&q=${quality || 75}`;
+  return `https://raw.githubusercontent.com/Jupyyter/nepune_place/main/public/imgs/${src}?w=${width}&q=${quality || 75}`;
 };
 
 export default imageLoader;
