ed54aa6e6c5a6f3d04cec35b3ee12962c8d2ae05
) <raducea.matei2005@gmail.com>
2024-08-04 05:39:37 +0300

    a

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
        output: 'export',
-       basePath: '/nepune_place',
+       images: {
+        unoptimized: true,
+      },
      }
      module.exports = nextConfig
