21933180fe4a7a61d75d4c2515aed85046e14f85
nepunePlace <raducea.matei2005@gmail.com>
2024-08-04 14:50:47 +0300

    k

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
        output: 'export',
        basePath: '/nepune_place',
        images: {
+        unoptimized: true,
         loader: 'custom',
         loaderFile: './imageLoader.ts',
       },
