3eda7de8ce77d099206099185a90425e53c53f5d
nepunePlace <raducea.matei2005@gmail.com>
2024-08-04 13:15:35 +0300

    g

--- app/Home.tsx
+++ app/Home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
            <main className="text-center p-4">
              <h1 className="text-4xl font-bold mb-4">nepune place</h1>
              <p className="text-lg">
-               i use this site to represent myself on the internet and showcase my projects. nothing much.
+               i use this site to represent myself on the internet and showcase my projects. nothing much..
              </p>
            </main>
          </div>
