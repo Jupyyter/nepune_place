e52326f87e43ea6e11631ae37f0c86b85db7b67c
nepunePlace <raducea.matei2005@gmail.com>
2024-08-05 08:01:33 +0300

    k

--- next.config.js
+++ next.config.js
@@ -1,19 +1,8 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = ''
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: '/nepune_place',
+  assetPrefix: '/nepune_place/',
 }
 
-module.exports = {
-  output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    unoptimized: true,
-  },
-}
+module.exports = nextConfig
