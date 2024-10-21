8447a9bca8826d5db64d0e24bdbfe6c786eb060c
nepunePlace <raducea.matei2005@gmail.com>
2024-10-22 02:49:19 +0300

    t

--- app/Menu.tsx
+++ app/Menu.tsx
@@ -14,16 +14,16 @@ const Menu: React.FC = () => {
 
   return (
     <nav>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-wrap justify-center items-center">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="flex flex-nowrap justify-between sm:justify-center items-center">
           {menuItems.map((item) => (
             <Link
               key={item.path}
               href={item.path}
-              className={`text-white px-12 py-4 rounded-md text-sm sm:text-base font-medium m-1  ${
+              className={`text-white px-2 sm:px-12 py-2 sm:py-4 rounded-md text-xs sm:text-base font-medium sm:m-1 whitespace-nowrap ${
                 pathname === item.path
-                  ? "bg-red-900" // Active button color
-                  : "bg-gray-600 hover:bg-yellow-500" // Inactive button color
+                  ? "bg-red-900"
+                  : "bg-gray-600 hover:bg-yellow-500"
               }`}
             >
               {item.label}
