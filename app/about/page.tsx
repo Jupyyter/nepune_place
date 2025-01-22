5dd75c91bd8d1abbfcc1e75858b14ab55ea829cf
nepunePlace <raducea.matei2005@gmail.com>
2025-01-22 05:32:40 +0200

    gg

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -20,7 +20,7 @@ const About = () => {
 
   const technologies = [
     { icon: <FaJava />, name: 'Java', comment: 'I learned the basics by making checkers and some other game in greenfoot. i hate the syntax' },
-    { icon: <SiCplusplus />, name: 'C/C++', comment: 'my favoryte programing language. can do anything with it. the program not eficient? skill issue' },
+    { icon: <SiCplusplus />, name: 'C/C++', comment: 'my favoryte programing language. can do anything with it. the program not efficient? skill issue' },
     { icon: <SiJavascript />, name: 'JavaScript', comment: '0 == [] (true); 0 == "0" (true); "0" == [] (false); do I need to say anything else?'},
     { icon: <SiTypescript />, name: 'TypeScript', comment: 'JavaScript but better. I used it to make this site' },
     { 
@@ -35,7 +35,7 @@ const About = () => {
         />
       ), 
       name: 'Greenfoot', 
-      comment: 'a joke of a game engine. the sound systom simply doesnt work properly for some reason. worst experience that i ever had using any software' 
+      comment: 'a joke of a game engine. the sound system simply doesnt work properly for some reason. worst experience that i ever had using any software' 
     },
     { icon: <SiSfml />, name: 'SFML', comment: 'practical graphics library when you want to make things appear on the screen. i choose it over sdl2 altho thats just because its more simple' },
     { 
@@ -52,7 +52,7 @@ const About = () => {
       name: 'SDL2', 
       comment: 'its ok. i had a good experience with sdl2, but i would only use it over sfml when developing android or ios apps. i dont really know why, but i think i simply dont like how the code looks' 
     },
-    { icon: <FaUnity />, name: 'Unity', comment: 'great engine. unfortunately due to all the ui, making a game in unity doesnt compare to making a game without an engine' },
+    { icon: <FaUnity />, name: 'Unity', comment: 'great engine. unfortunately due to all the ui, making a game in unity doesnt compare to making a game without an engine. besides, its not open source' },
     { icon: <FaReact />, name: 'React', comment: 'i used nextjs which uses react for this site, so react=yes=i do indeed like it over the other site-making-software' },
     { icon: <FaHtml5 />, name: 'HTML', comment: 'umm...do i have to say that i do know some html since i made this site?'},
     { icon: <FaCss3Alt />, name: 'CSS', comment: 'yes, i can code some css since i made this site' },
@@ -68,7 +68,7 @@ const About = () => {
         />
       ), 
       name: 'Godot', 
-      comment: 'great engine. i like it more than unity because its more lightweight and the code is more fun to write' 
+      comment: 'great engine. i like it more than unity because its more lightweight and its open source (hooray)' 
     },
     { 
       icon: (
@@ -82,7 +82,7 @@ const About = () => {
         />
       ), 
       name: 'GDScript', 
-      comment: 'the language used in godot. its like python but better' 
+      comment: 'the language used in godot. its like python but better :)' 
     },
     { icon: <SiNextdotjs />, name: 'Next.js', comment: 'used it to make this site. its great for making websites with react' }
   ];
